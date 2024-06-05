168f3d5c6b321df78027c29a7d56f04a22155ed2
Pablo Aldana <pabloaldana.cipo@gmail.com>
2024-06-05 09:56:12 -0300

    elimanar post terminado con sweetalert2

--- resources/views/areas/index.blade.php
+++ resources/views/areas/index.blade.php
@@ -18,6 +18,7 @@
                     <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Nros de post:</span>
                 </div>
             </a>
+
         </div>
         @endforeach
     </div>
