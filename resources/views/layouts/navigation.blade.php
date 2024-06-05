3029cb801e44707c63ee6049634177a770c44ed6
Pablo Aldana <pabloaldana.cipo@gmail.com>
2024-06-05 10:52:37 -0300

    ultimo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -26,6 +26,11 @@
                         {{ __('Mis Posteos') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.index')">
+                        {{ __('Mis Areas') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
