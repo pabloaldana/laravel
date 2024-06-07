3ca88d0de37621ee6551cfe1112fe56225d79210
Pablo Aldana <pabloaldana.cipo@gmail.com>
2024-06-07 18:32:00 -0300

    editar,eliminar y crear de post listos y testeado

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -10,8 +10,10 @@
         <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 bg-white dark:bg-gray-800 border-b border-gray-200">
                 <h1 class="text-2xl font-bold mb-6 text-gray-800 dark:text-gray-200">Formulario del Posteo</h1>
-                <form method="POST" id="edit-post-form" action="{{ route('posts.store') }}">
+                <form method="POST" id="edit-post-form" action="{{ route('posts.update', $post->id) }}">
                     @csrf
+                    @method('PUT') <!-- Este método indica que es una actualización -->
+
                     <div class="mb-4">
                         <label for="titulo" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Titulo:</label>
                         <input type="text" id="titulo" name="titulo" required class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-700 dark:border-gray-600 dark:text-gray-300" value="{{ $post->titulo }}">
@@ -34,8 +36,9 @@
                         </div>
 
                         <label for="texto" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Contenido:</label>
-                        <textarea type="text" id="texto" name="texto" required class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-700 dark:border-gray-600 dark:text-gray-300">{{ $post->texto }}</textarea>
+                        <textarea id="texto" name="texto" required class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-700 dark:border-gray-600 dark:text-gray-300">{{ $post->texto }}</textarea>
                     </div>
+
                     <div class="flex items-center justify-end mt-4">
                         <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                             Modificar
@@ -45,6 +48,7 @@
                         </button>
                     </div>
                 </form>
+
             </div>
         </div>
     </div>
