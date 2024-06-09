400ec08ae37066c7af85bcaa529ff2a68cd74df2
Percy Alvarez <praa.nqn@gmail.com>
2024-06-09 12:15:15 -0300

    component boton-volver

--- resources/views/components/boton-volver.blade.php
+++ resources/views/components/boton-volver.blade.php
@@ -1,5 +1,5 @@
 <div class="flex items-center justify-center ">
-    <button onclick="history.back()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded md:py-4 md:px-8 lg:py-6 lg:px-12 lg:text-2xl">
+    <button onclick="history.back()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded md:py-4 md:px-8 lg:py-6 lg:px-12 lg:text-2xl m-[10px]">
         Volver
     </button>
 </div>
